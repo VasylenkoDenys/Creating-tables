05ba975ae4242272090ed9fa837135ce493bd2e6
VasylenkoDenys <vasylenkodenys92@gmail.com>
2023-01-06 00:23:13 +0200

    fixes

--- sql/workers/table-task.sql
+++ sql/workers/table-task.sql
@@ -2,7 +2,7 @@ CREATE TABLE workers (
   id serial PRIMARY KEY,
   birthday date NOT NULL CHECK (birthday < current_date),
   name varchar(20) NOT NULL CHECK (name != ''),
-  salary numeric (6, 2) NOT NULL CHECK (salary >= 0) DEFAULT 0,
+  salary numeric (6, 2) CHECK (salary >= 0) DEFAULT 0,
   created_at timestamp NOT NULL DEFAULT current_timestamp,
   updated_at timestamp NOT NULL DEFAULT current_timestamp
 );
